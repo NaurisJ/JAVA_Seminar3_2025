058eb2ccb6630614703fe66642b4ead950048e07
NaurisJ <nauris.javalds@gmail.com>
2025-02-26 11:20:05 +0200

    ADD: added GuestUser as model class

--- src/model/GuestUser.java
+++ src/model/GuestUser.java
@@ -1,5 +1,38 @@
 package model;
 
 public class GuestUser {
-
+	//mainigie
+	private long generatedID;
+	
+	private static long counter = 0;
+	
+	
+	// getter
+	public long getGeneratedID() {
+		return generatedID;
+	}
+	
+	
+	// setter
+	public void setGeneratedID() {
+		generatedID = counter++;
+	}
+	
+	
+	// konstruktors
+	
+	public GuestUser() {
+		setGeneratedID();
+	}
+	
+	
+	@Override
+	
+	public String toString() {
+		return "ID: " + generatedID + " ";
+	}
+	
+	
+	//TODO pabeigt ar filtraciajs funkkcija
+	
 }
